fc3cb371685035ecdca5f169f64a584b1147b844
mpio <mpiotro4@outlook.com>
2023-05-06 13:17:36 +0200

    first commit

---
+++ src/main/java/com/example/workflow/ReserveSeatOnBoat.java
@@ -0,0 +1,31 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import javax.inject.Named;
+
+@Named
+public class ReserveSeatOnBoat implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        String money = "0.0";
+        String ticketType = "Coach";
+
+        money = (String) delegateExecution.getVariable("money");
+        double moneyDouble = Double.parseDouble(money);
+
+        if (moneyDouble >= 10000) {
+            ticketType = "First Class";
+        } else if (moneyDouble >= 5000) {
+            ticketType = "Business Class";
+        } else if (moneyDouble <= 10) {
+            ticketType = "Stowaway Class";
+            throw new BpmnError("Fall_overboard", "A cheap ticket led to a small wave throwing you overboard");
+        }
+
+        delegateExecution.setVariable("ticketType", ticketType);
+    }
+}
